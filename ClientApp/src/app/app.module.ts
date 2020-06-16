f31abbb6c1c736ee078d81925d0d5ba7414e03bb
candypops <mariagabriela236@gmail.com>
2020-06-16 00:36:30 -0400

    actualizados los imports

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NavMenuService } from './services/nav-menu.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -9,7 +10,6 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
@@ -21,15 +21,14 @@ import { VentanaModalComponent } from './ventana-modal/ventana-modal.component';
 import { VistaPerfilComponent } from './vista-perfil/vista-perfil.component';
 import { TablaMovimientosComponent } from './tabla-movimientos/tabla-movimientos.component';
 import { BloquearComponent } from './bloquear/bloquear.component';
-import { TransferirComponent } from '../transferir/transferir.component';
-import { TransferirCardComponent } from '../transferir-card/transferir-card.component';
+import { TransferirCardComponent } from './transferir-card/transferir-card.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
     SignupFormComponent,
     LoginFormComponent,
     TopNavComponent,
@@ -40,8 +39,7 @@ import { TransferirCardComponent } from '../transferir-card/transferir-card.comp
     VentanaModalComponent,
     VistaPerfilComponent,
     TablaMovimientosComponent,
-    BloquearComponent,
-    TransferirComponent,
+    BloquearComponent, 
     TransferirCardComponent
   ],
   imports: [
